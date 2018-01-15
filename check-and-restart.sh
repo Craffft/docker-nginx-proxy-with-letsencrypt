3d6bcc0e30ae47477424bd9b2a7e98f94d8dfdbd
Daniel Kiesel <icodr8@gmail.com>
2018-01-15 21:10:25 +0100

    optimized check and restart script

--- check-and-restart.sh
+++ check-and-restart.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-CONTAINERS=( "nginx" "nginx-gen" "letsencrypt-nginx-proxy-companion")
+CONTAINERS=( "nginx" "nginx-gen" "letsencrypt-nginx-proxy-companion" )
 
-for i in $CONTAINERS
-do
+for i in "${CONTAINERS[@]}"; do
     echo "Check container \"$i\""
     IS_RUNNING="$(docker inspect -f {{.State.Running}} $i)"
 
     if [ ! $IS_RUNNING = true ]; then
         echo "RESTART \"$i\" Container!"
-        # docker start nginx-gen
+        echo "$(docker start $i)"
     else
         echo "\"$i\" is already running!"
     fi
